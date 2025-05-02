9af699f8c72ca20f5cf450896e12903c38444149
kartikm39 <manchandakartik11@gmail.com>
2025-05-03 01:33:51 +0530

    Added TSConfig Files

---
+++ apps/frontend/src/components/Show.tsx
@@ -0,0 +1,10 @@
+interface Props{
+    show: boolean,
+    children: React.ReactNode
+}
+
+const Show: React.FC<Props> = ({ show, children}: Props): React.ReactNode => {
+
+}
+
+export default Show;
